4de3552760b82ac0b3fdb321641f5490084b2e85
eduardo <dudumelgaco@hotmail.com>
2019-09-07 18:18:19 -0300

    Add ingredientsapp, models and migration

---
+++ ingredientsapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IngredientsappConfig(AppConfig):
+    name = 'ingredientsapp'
---
+++ ingredientsapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+
+class Ingredient(models.Model):
+    GRAMS = 'G'
+    KILOGRAMS = 'KG'
+    LITER = 'L'
+    CENTILITER = 'CL'
+    UNIT_TYPES = (
+        (GRAMS, 'Grams (g)'),
+        (KILOGRAMS, 'Kilograms (kg)'),
+        (LITER, 'Liter (L)'),
+        (CENTILITER, 'Centiliter (cl)'),
+    )
+    name = models.CharField(max_length=200, null=False, blank=False)
+    article_number = models.CharField(max_length=13, null=False, blank=False, unique=True)
+    cost = models.DecimalField(max_digits=6, decimal_places=2)
+    amount = models.PositiveIntegerField()
+    units = models.CharField(max_length=200, null=False, blank=False, choices=UNIT_TYPES)
+
+
+    def __str__(self):
+        return f'{self.name} - {self.article_number}'
